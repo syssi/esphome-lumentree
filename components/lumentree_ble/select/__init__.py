468052ad8884e733cc40c3abd1ef328d3f30b670
Sebastian Muszynski <basti@linkt.de>
2026-03-25 17:06:28 +0100

    Use new_* API instead of deprecated register_* functions
    Replace register_binary_sensor/register_switch/register_button/register_number/register_select
    with the modern new_* API. The new_* functions internally call cg.new_Pvariable() + register_*(),
    reducing boilerplate. register_component() is kept separately for Component subclasses.

--- components/lumentree_ble/select/__init__.py
+++ components/lumentree_ble/select/__init__.py
@@ -34,11 +34,10 @@ async def to_code(config):
     hub = await cg.get_variable(config[CONF_LUMENTREE_BLE_ID])
     if CONF_OPERATION_MODE in config:
         conf = config[CONF_OPERATION_MODE]
-        var = cg.new_Pvariable(conf[cv.CONF_ID])
-        await cg.register_component(var, conf)
-        await select.register_select(
-            var, conf, options=list(OPERATION_MODE_OPTIONS.keys())
+        var = await select.new_select(
+            conf, options=list(OPERATION_MODE_OPTIONS.keys())
         )
+        await cg.register_component(var, conf)
         cg.add(var.set_parent(hub))
         cg.add(var.set_holding_register(150))  # Work Mode Setting register
 
