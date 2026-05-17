62d8e0a78e42114acba90e69aaf9c87d53508dc6
Sebastian Muszynski <basti@linkt.de>
2026-05-17 19:49:20 +0200

    Apply pre-commit fixes (ruff-format)

--- components/lumentree_ble/sensor.py
+++ components/lumentree_ble/sensor.py
@@ -247,7 +247,10 @@ SENSOR_DEFS = {
 }
 
 CONFIG_SCHEMA = LUMENTREE_BLE_COMPONENT_SCHEMA.extend(
-    {cv.Optional(key): sensor.sensor_schema(**kwargs) for key, kwargs in SENSOR_DEFS.items()}
+    {
+        cv.Optional(key): sensor.sensor_schema(**kwargs)
+        for key, kwargs in SENSOR_DEFS.items()
+    }
 )
 
 
